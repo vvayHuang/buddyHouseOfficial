a5d6fc1ac86e4b89656b5fa1d62a073fada1cba4
wayHuang <88394654+vvayHuang@users.noreply.github.com>
2025-11-14 14:20:21 +0800

    Update package dependencies, enhance swiper responsiveness, and refine HTML structure. Upgraded Vite to version 5.0.0 and added breakpoints for swiper. Improved accessibility and layout in index.html and added utilities.scss import in all.scss.

--- swiper.js
+++ swiper.js
@@ -8,6 +8,23 @@ const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
     slidesPerView: "4",
+    breakpoints: {
+        // when window width is >= 320px
+        375: {
+            slidesPerView: 2,
+            spaceBetween: 16
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+    },
     speed: 10000,
     autoplay: {
         delay: 0,
